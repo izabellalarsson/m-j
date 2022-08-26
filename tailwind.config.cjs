3f5f84378ae81a2d208abf16eb36e5d3e5dbfd55
izabellalarsson <izzwii.izabella@gmail.com>
2022-08-26 09:32:16 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+	colors: {
+	background: '#F2E3D5'
+	seconndary: '#026773'
+	onBackground: '#3CA6A6'
+	onBackground: '#024959'
+	onBackground: '#3CA6A6'
+	}
+    },
+  },
+  plugins: [],
+}
