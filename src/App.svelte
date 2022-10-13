ce0b79cc8a24df38272348b991b78cf324d25dbc
izabellalarsson <izzwii.izabella@gmail.com>
2022-10-13 18:01:18 +0200

    something

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
-  import monstera from './assets/monstera.svg'
+  var countDownDate = new Date("May 27, 2023 12:00:00").getTime();
+  var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("demo").innerHTML = days + "dagar " + hours + "timmar "
+  + minutes + "minuter";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 </script>
 
 <main class="w-screen">
-  <div class="w-full flex justify-center items-end relative mb-36 mt-24">
-       <img class="h-[214px] absolute md:left-auto left-[-21px] bottom-[58px]" src={monstera} alt="Monstera plant" />
-<div class="pb-5 w-full bg-accent flex justify-end items-center h-60 flex-col pr-7">
-<div class="pl-48 md:pl-80">
-<h1 class="text-4xl flex flex-col text-primary">Madeleine <span class="self-center">& Jonas<span></h1>
-<h4 class="text-2xl text-center text-secondary">27.5.2023</h4>
-</div>
-</div>
+  <div class="w-full flex justify-center items-end relative mb-36 mt-16 flex-col items-center">
+    <div class="pb-5 w-full bg-accent flex justify-end items-center flex-col">
+      <div class="">
+        <h1 class="text-4xl flex flex-col text-primary">Madeleine <span class="self-center">& Jonas<span></h1>
+          <h4 class="text-2xl text-center text-secondary">27.5.2023</h4>
+      </div>
+    </div>
+    <div id="demo"/>
   </div>
+<div class="flex flex-col items-center gap-4">
+  <p>Osa till bröllopet:</p>
+  <a href="https://forms.gle/yDJdt3rPHDdmYmpq8" class="bg-secondary-secondary rounded-full p-4">Klicka här</a>
+</div>
+
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-	fontFamily: {
-	  italiana: ['Italiana', 'serif'],
-	},
-	colors: {
-	accent: '#F2E3D5',
-	seconndary: '#026773',
-	secondary: '#3CA6A6',
-	primary: '#024959',
-	onBackground: '#3CA6A6',
-	}
+      fontFamily: {
+        italiana: ['Italiana', 'serif'],
+      },
+      colors: {
+        primary: {
+          primary: '#f2e3d4',
+          light: '#ffffff',
+          dark: '#bfb1a3',
+        },
+        secondary: {
+          secondary: '#3ca6a6',
+          light: '#73d8d7',
+          dark: '#007777',
+        },
+        grey: '#323232',
+      },
     },
   },
   plugins: [],
