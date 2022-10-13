7937c30ff94a068a59a09bb3003c5fda58e95dea
Izabella Larsson <izzwii.izabella@gmail.com>
2022-10-13 18:02:39 +0200

    Basic layout (#1)
    * add italiana font to project
    
    * update colors
    
    * basic layout
    
    * something

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,36 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  var countDownDate = new Date("May 27, 2023 12:00:00").getTime();
+  var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("demo").innerHTML = days + "dagar " + hours + "timmar "
+  + minutes + "minuter";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="w-screen">
+  <div class="w-full flex justify-center items-end relative mb-36 mt-16 flex-col items-center">
+    <div class="pb-5 w-full bg-accent flex justify-end items-center flex-col">
+      <div class="">
+        <h1 class="text-4xl flex flex-col text-primary">Madeleine <span class="self-center">& Jonas<span></h1>
+          <h4 class="text-2xl text-center text-secondary">27.5.2023</h4>
+      </div>
+    </div>
+    <div id="demo"/>
   </div>
-  <h1>Vite + Svelte</h1>
+<div class="flex flex-col items-center gap-4">
+  <p>Osa till bröllopet:</p>
+  <a href="https://forms.gle/yDJdt3rPHDdmYmpq8" class="bg-secondary-secondary rounded-full p-4">Klicka här</a>
+</div>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
