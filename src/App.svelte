14100252fba0bdec602b2208a92dde2ad12e5cf0
izabellalarsson <izzwii.izabella@gmail.com>
2022-10-30 20:43:35 +0100

    update link and info

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,38 @@
 <script lang="ts">
-//   var countDownDate = new Date("May 27, 2023 12:00:00").getTime();
-//   var x = setInterval(function() {
-//   var now = new Date().getTime();
-//   var distance = countDownDate - now;
-    
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   var countDownDate = new Date("May 27, 2023 12:00:00").getTime();
+  //   var x = setInterval(function() {
+  //   var now = new Date().getTime();
+  //   var distance = countDownDate - now;
 
-//   document.getElementById("demo").innerHTML = days + "dagar " + hours + "timmar "
-//   + minutes + "minuter";
+  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
+  //   document.getElementById("demo").innerHTML = days + "dagar " + hours + "timmar "
+  //   + minutes + "minuter";
+
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //     document.getElementById("demo").innerHTML = "EXPIRED";
+  //   }
+  // }, 1000);
 </script>
 
 <main class="w-screen h-screen bg-secondary">
-  <div class="pb-5 w-full bg-accent grid grid-rows-3 h-full justify-center items-center">
+  <div
+    class="pb-5 w-full bg-accent grid grid-rows-3 h-full justify-center items-center"
+  >
     <h4 class="text-2xl text-center text-primary">27.5.2023</h4>
-    <h1 class="text-4xl flex flex-col text-primary">Madeleine & Jonas</h1>
-    <a href="#" class="border-2 text-primary border-primary py-2 w-fit justify-self-center px-8 text-center">OSA NU</a>
+    <div class="flex flex-col items-center">
+      <h1 class="text-4xl flex flex-col text-primary">Madeleine & Jonas</h1>
+      <p class="text-primary">Mer info kommer inom kort 🥳</p>
+    </div>
+    <a
+      href="https://forms.gle/naomyrixb7FnChJKA"
+      class="border-2 text-primary border-primary py-2 w-fit justify-self-center px-8 text-center"
+      >OSA NU</a
+    >
   </div>
 
-<div class="flex flex-col items-center gap-4">
-</div>
-
+  <div class="flex flex-col items-center gap-4" />
 </main>
