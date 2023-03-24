0691c7b12ba7a3d7b29755fe146e56bafbfc80d5
Izabella Larsson <izzwii.izabella@gmail.com>
2023-03-24 19:43:42 +0100

    feat: add new gifts section (#3)

--- src/App.svelte
+++ src/App.svelte
@@ -6,23 +6,7 @@
   import Weddingday from "./lib/Weddingday.svelte";
   import Location from "./lib/Location.svelte";
   import Navigation from "./lib/Navigation.svelte";
-  //   var countDownDate = new Date("May 27, 2023 12:00:00").getTime();
-  //   var x = setInterval(function() {
-  //   var now = new Date().getTime();
-  //   var distance = countDownDate - now;
-
-  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
-  //   document.getElementById("demo").innerHTML = days + "dagar " + hours + "timmar "
-  //   + minutes + "minuter";
-
-  //   if (distance < 0) {
-  //     clearInterval(x);
-  //     document.getElementById("demo").innerHTML = "EXPIRED";
-  //   }
-  // }, 1000);
+  import Gifts from "./lib/Gifts.svelte";
 </script>
 
 <main class="w-screen bg-secondary">
@@ -32,5 +16,6 @@
   <Speech />
   <Location />
   <Faq />
+  <Gifts />
   <Questions />
 </main>
