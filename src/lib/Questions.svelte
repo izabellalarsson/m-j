214068b5793f90207f9d80a80dfe0edbb8498fd3
Izabella Larsson Polestar <izabella.larsson@polestar.com>
2023-03-24 21:09:09 +0100

    Revert "feat: add new gifts section (#3)"
    This reverts commit 0691c7b12ba7a3d7b29755fe146e56bafbfc80d5.

--- src/lib/Questions.svelte
+++ src/lib/Questions.svelte
@@ -2,12 +2,10 @@
   import Layout from "./Layout.svelte";
 </script>
 
-<Layout state="inverted" id="questions">
-  <section
-    class="grid grid-rows-2 h-full place-items-center selection:bg-secondary selection:text-primary"
-  >
-    <h2 class="text-accent">Övriga frågor</h2>
-    <p class="text-white text-center">
+<Layout id="questions">
+  <section class="flex flex-col items-end">
+    <h2>Övriga frågor</h2>
+    <p class="text-right flex flex-col">
       Är det något ni undrar över är ni givetvis välkomna att prata med
       brudparet i fråga.
       <span
@@ -21,7 +19,7 @@
     </p>
   </section>
   <section class="mt-40">
-    <h3 class="text-accent">
+    <h3>
       Psst.. missade du OSA-knappen? <a
         href="https://docs.google.com/forms/d/e/1FAIpQLScsUx1mpPeYf7NSfDcxdFwG_deFxup3EWyZCVw9vmQ_TXw6Og/viewform?usp=sf_link"
         >Klicka här för att komma till formuläret</a
